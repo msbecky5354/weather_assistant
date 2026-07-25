f94ca0cbb9eea8901f9fa08458791c2eba5002b4
msbecky5354 <msbgpt5354@gmail.com>
2026-07-26 00:01:55 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// ⚠️ 請填入你嘅 Supabase 專案網址同埋 Anon Key
+const SUPABASE_URL = 'https://oylemsbvdcxqawmrufxt.supabase.co'; 
+const SUPABASE_ANON_KEY = 'sb_publishable_BbUDz-IRGziPBHqyHfkg_A_DveczOdA'; 
